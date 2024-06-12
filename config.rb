327ffe98d56822e58b72091f9522213585db1548
Jan Lelis <hi@syntaxtransformation.com>
2024-06-12 11:57:41 +0200

    Add Ruby 3.3.3

--- config.rb
+++ config.rb
@@ -20,9 +20,10 @@ JRUBY_SOURCE_PREFIX = "https://github.com/jruby/jruby/tree/master/"
 
 STDGEMS_VERSION = JSON.parse(DEFAULT_GEMS_FILE)["version"]
 
-CURRENT_RUBY_VERSION = '3.3.2'
+CURRENT_RUBY_VERSION = '3.3.3'
 
 RUBY_3_3_VERSIONS = %w[
+  3.3.3
   3.3.2
   3.3.1
   3.3.0
